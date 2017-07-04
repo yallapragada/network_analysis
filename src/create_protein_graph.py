4323107e4e407644da02bc2e021b35d7c3e964c3
yallapragada <uyallapragada@gmail.com>
2017-07-04 11:25:00 -0400

    added comments to explain usage of this script

--- src/create_protein_graph.py
+++ src/create_protein_graph.py
@@ -1,3 +1,9 @@
+'''
+this script creates protein level plots from residue-level graphml plots
+usage:
+python create_protein_graph.py [folder for input graphml files and to write output plots] [name of graphml] [title for plot]
+'''
+
 #input: graphml of correlated mutations
 #output: plot of protein level graph depicted nodes/edges
 #for each of the 10 proteins
@@ -10,25 +16,35 @@ from graph_analysis_util import read_graphml, write_macro_graphml, get_top_node,
 import matplotlib
 import sys, os
 
-
-#macro plot
+#crete protein-level macro plot
 def create_plot(g, folder, plot_file, title):
     plt.style.use(['seaborn-white', 'seaborn-paper'])
     matplotlib.rc("font", family="Times New Roman")
 
-    colors = ['r', 'b', 'g', 'c', 'm', 'r', 'b', 'g', 'c', 'm']
-    graph_pos=nx.shell_layout(sorted(g.nodes()))
+    sorted_nodes = sorted(g.nodes())
+    print(sorted_nodes)
+    graph_pos=nx.shell_layout(sorted_nodes)
+    print(graph_pos.keys())
 
-    labels = [node+'_'+str(g.node[node]['count']) for node in g.nodes_iter()]
-    labelsD = {node:node+'('+str(g.node[node]['count'])+')' for node in g.nodes_iter()}
-
-    #g2=nx.relabel_nodes(g,labelsD)
-
+    labelsD = {node: node + '(' + str(g.node[node]['count']) + ')' for node in sorted_nodes}
     weights = [d['weight'] if 'weight' in d else 0 for u, v, d in g.edges(data=True)]
     norm_weights = [10.0*(float(i)-min(weights))/max(weights) for i in weights]
 
+    val_map = {'ha': 'Crimson',
+               'na': 'Cyan',
+               'm1': 'DarkOrange',
+               'm2': 'Gold',
+               'np': 'LawnGreen',
+               'pa': 'RoyalBlue',
+               'pb1': 'Magenta',
+               'pb2': 'HotPink',
+               'ns1': 'SlateGrey',
+               'ns2': 'Purple'}
+
+    values = [val_map.get(node) for node in g.nodes()]
+
     # draw graph
-    nx.draw_networkx_nodes(g,graph_pos, node_size=2400, node_color=colors)
+    nx.draw_networkx_nodes(g,graph_pos, node_size=2400, node_color=values)
     nx.draw_networkx_edges(g,graph_pos,width=norm_weights)
     nx.draw_networkx_labels(g,graph_pos, labelsD)
 
@@ -40,6 +56,9 @@ def create_plot(g, folder, plot_file, title):
     plt.close()
 
 def create_protein_graph(ingraph):
+
+    proteins = ['ha', 'm2', 'ns2', 'pa', 'np', 'na', 'pb1', 'm1', 'pb2', 'ns1']
+
     pnodes = {}
     pnodes = defaultdict(lambda: 0, pnodes)
 
@@ -50,6 +69,12 @@ def create_protein_graph(ingraph):
 
     for node in ingraph.nodes_iter():
         pnodes[ingraph.node[node]['protein']]+=1
+
+    for protein in proteins:
+        if protein not in pnodes:
+            pnodes[protein]=0
+
+    print('pnodes ', pnodes)
 
     for u,v,d in ingraph.edges(data=True):
         key=(ingraph.node[u]['protein'], ingraph.node[v]['protein'])
@@ -74,6 +99,7 @@ def run(folder, infilename, title):
     write_macro_graphml(outgraph, folder, infilename)
     plot_file = infilename.split('.')[0] + '.png'
     create_plot(outgraph, folder, plot_file, title)
+
 
 if __name__ == '__main__':
     folder     = sys.argv[1]
